c0f0ce5b3e1dfe9b33d990486f3fc720b6e79ded
L-2022 <bilyakvasya@ukr.net>
2023-03-24 11:22:12 +0200

    A one-page site that displays 5 parameters about the user

--- server/routes/userRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const userController = require('../controllers/userController')
-
-
-router.get('/users', userController.getUsers)
-
-
-module.exports = router
